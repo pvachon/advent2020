9e7f7c84f2ff555d97ce90f2428b672bd5a17884
Phil Vachon <phil@security-embedded.com>
2020-12-19 09:49:59 -0500

    Add day 5

---
+++ 5.0/fifth.adb
@@ -0,0 +1,80 @@
+with Ada.Text_IO,
+     Ada.Command_Line;
+
+procedure Fifth is
+    package Text_IO renames Ada.Text_IO;
+    package Command_Line renames Ada.Command_Line;
+
+    file : Text_IO.File_Type;
+
+    function Partition_Walk(loc : in String;
+                            min, max : in Integer;
+                            upper, lower : in Character) return Integer is
+        part_min : Integer := 0;
+        part_max : Integer := max;
+        part_count : Integer := (max - min) + 1;
+    begin
+        -- Text_IO.Put_Line("  DEBUG: Spec: " & loc);
+        for I in loc'Range loop
+            part_count := part_count / 2;
+            if loc(I) = lower then
+                part_max := part_max - part_count;
+            elsif loc(I) = upper then
+                part_min := part_min + part_count;
+            else
+                Text_IO.Put_Line("Invalid direction spec: " & loc(I));
+            end if;
+            --Text_IO.Put_Line("  DEBUG: Count: " & Integer'Image(part_count) &
+            --    " Max: " & Integer'Image(part_max) &
+            --    " Min: " & Integer'Image(part_min));
+        end loop;
+
+        return part_max;
+
+    end Partition_Walk;
+
+    function Parse_Line(cur_loc : in String) return Integer is
+        row : Integer := 0;
+        col : Integer := 0;
+    begin
+        -- Walk the row location
+        row := Partition_Walk(cur_loc(cur_loc'First..cur_loc'First + 6), 0, 127, 'B', 'F');
+
+        -- Walk the column location
+        col := Partition_Walk(cur_loc(cur_loc'First + 7..cur_loc'First + 9), 0, 7, 'R', 'L');
+
+        Text_IO.Put_Line("Location (" & cur_loc & "): (" & Integer'Image(row) &
+            "," & Integer'Image(col) &
+            ")");
+
+        return row * 8 + col;
+    end Parse_Line;
+
+    seat_id : Integer;
+    max_seat_id : Integer := 0;
+begin
+
+    Text_IO.Put_Line("Advent of Code: 5.0");
+
+    if Command_Line.Argument_Count /= 1 then
+        Text_IO.Put_Line("Need to specify file of boarding passes. Aborting.");
+        return;
+    end if;
+
+	Text_IO.Put_Line("File name: " & Command_Line.Argument(1));
+
+    Text_IO.Open(File => file,
+                 Mode => Text_IO.In_File,
+                 Name => Command_Line.Argument(1));
+
+    while not Text_IO.End_Of_File(file) loop
+        seat_id := Parse_Line(Text_IO.Get_Line(file));
+        if seat_id > max_seat_id then
+            max_seat_id := seat_id;
+        end if;
+    end loop;
+
+    Text_IO.Put_Line("Maximum seat ID: " & Integer'Image(max_seat_id));
+
+end Fifth;
+
